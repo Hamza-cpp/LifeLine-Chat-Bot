ccf48c69cbf5ad8a9c9c8668ce03569e42d85c4b
OKHADIR Hamza <hamza.okhadir2018@gmail.com>
2024-09-29 21:09:23 +0100

    💬: The Twilio message API was configured successfully

--- src/app.py
+++ src/app.py
@@ -11,3 +11,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+load_dotenv()
+
+# Your Account SID and Auth Token from console.twilio.com
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="whatsapp:+212620661864",
+    from_="whatsapp:+14155238886",
+    body="Hello frhhhhhhhhhhhhhhhhhhhhhhom Python!",
+)
+
+print(message.sid)
